66552dc11fc25080db914e1a4e580d9dc2fb555e
namancoder <namanitsofficial@gmail.com>
2021-02-16 14:12:22 +0530

    tic

--- Tic-Tac-Toe-Game-Flutter-master/lib/main.dart
+++ Tic-Tac-Toe-Game-Flutter-master/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
-import 'home_page.dart';
 import 'double_hom_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,69 +24,124 @@ class FirstScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text('Tic Tac Toe'),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, 100),
+        child: Container(
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(color: Colors.black12, spreadRadius: 5, blurRadius: 2)
+          ]),
+          width: MediaQuery.of(context).size.width,
+          height: 100,
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.purple[900],
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20))),
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Tic Tac Toe",
+                    style: GoogleFonts.oswald(
+                      fontSize: 40,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[Row(mainAxisAlignment: MainAxisAlignment.center,children: [Text("Tic Tac Toe Game",style: TextStyle(fontSize: 40,color: Colors.amber,),)],),
-          SizedBox(height: 20,),
-          Container(child: CircleAvatar(
+        children: <Widget>[
+          Container(
+            child: CircleAvatar(
                 radius: 210,
                 backgroundColor: Colors.transparent,
-                backgroundImage: NetworkImage(
-                    'https://media.giphy.com/media/KzKFAvaM1RBoRU5dcl/giphy.gif'),
-              ),),
-          SizedBox(height: 20),
-          DropdownButton(
-            dropdownColor: Colors.red,
-            value: _selectedPlayer,
-            items: _dropDownItem(),
-            onChanged: (value) {
-              _selectedPlayer = value;
-              switch (value) {
-                case "Single Player":
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                  break;
-                case "Double Player":
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => DoubleHomePage()),
-                  );
-                  break;
-              }
-            },
-            hint: Container(
-              alignment: Alignment.centerRight,
-              width: 210,
-              child: Text(
-                'Select Player',
-                textAlign: TextAlign.end,
-                style: TextStyle(backgroundColor: Colors.amber),
-              ),
-            ),
+                backgroundImage: ExactAssetImage(
+                    'assets/images/javascript-tic-tac-toe-tutorial-react.png')),
           ),
+          SizedBox(height: 20),
+          ListTile(
+            title: Row(
+              children: <Widget>[
+                Expanded(
+                    child: RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                  child: Text("Computer"),
+                  color: Colors.black,
+                  textColor: Colors.white,
+                )),
+                Expanded(
+                    child: RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DoubleHomePage()),
+                    );
+                  },
+                  child: Text("Versus"),
+                  color: Colors.black,
+                  textColor: Colors.white,
+                )),
+              ],
+            ),
+          )
+          // DropdownButton(
+          //   dropdownColor: Colors.red,
+          //   value: _selectedPlayer,
+          //   items: _dropDownItem(),
+          //   onChanged: (value) {
+          //     _selectedPlayer = value;
+          //     switch (value) {
+          //       case "Single Player":
+          //         Navigator.push(
+          //           context,
+          //           MaterialPageRoute(builder: (context) => HomePage()),
+          //         );
+          //         break;
+          //       case "Double Player":
+          //         Navigator.push(
+          //           context,
+          //           MaterialPageRoute(builder: (context) => DoubleHomePage()),
+          //         );
+          //         break;
+          //     }
+          //   },
+          //   hint: Container(
+          //     alignment: Alignment.centerRight,
+          //     width: 210,
+          //     child: Text(
+          //       'Select Player',
+          //       textAlign: TextAlign.end,
+          //       style: TextStyle(backgroundColor: Colors.amber),
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
   }
 
-  List<DropdownMenuItem<String>> _dropDownItem() {
-    List<String> ddl = ["Single Player", "Double Player"];
-    return ddl
-        .map((value) => DropdownMenuItem(
-              value: value,
-              child: Text(value),
-            ))
-        .toList();
-  }
+  // List<DropdownMenuItem<String>> _dropDownItem() {
+  //   List<String> ddl = ["Single Player", "Double Player"];
+  //   return ddl
+  //       .map((value) => DropdownMenuItem(
+  //             value: value,
+  //             child: Text(value),
+  //           ))
+  //       .toList();
+  // }
 }
-
-
-
-
