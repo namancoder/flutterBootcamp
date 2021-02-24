f5ba53946334abd27904d7f26ef9a30791d7a8ff
namancoder <namanitsofficial@gmail.com>
2021-02-24 15:36:18 +0530

    sqflite 2

--- SQLITE_MVP/sqlite_login/lib/data/database_helper.dart
+++ SQLITE_MVP/sqlite_login/lib/data/database_helper.dart
@@ -1,11 +1,67 @@
-import 'package:sqflite/sqflite.dart';
+// import 'package:sqflite/sqflite.dart';
+// import 'dart:async';
+// import 'dart:io' as io;
+// import 'package:path_provider/path_provider.dart';
+// import 'package:sqlite_login/models/user.dart';
+
+// class DatabaseHelper {
+//   static final DatabaseHelper _instance = new DatabaseHelper.internal();
+
+//   factory DatabaseHelper() => _instance;
+//   static Database _db;
+
+//   Future<Database> get db async {
+//     if (_db != null) {
+//       return _db;
+//     }
+//     _db = await initDb();
+//     return _db;
+//   }
+
+//   DatabaseHelper.internal();
+
+//   initDb() async {
+//     io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+//     String path = join(documentDirectory.path, "main.db");
+//     var ourDb = await openDatabase(path, version: 1, oncreate: _onCreate);
+//     return ourDb;
+//   }
+
+//   void _onCreate(Database db, int version) async {
+//     await db.execute(
+//         "CREATE TABLE User(id INTEGER PRIMARY KEY, username TEXT, password TEXT");
+//     print("TAbel is  created");
+//   }
+
+//   //insertion plwase
+
+//   Future<int> saveUser(User user) async {
+//     var dbClient = await db;
+//     int res = await dbClient.insert("User", user.toMap());
+//     return res;
+//   }
+
+//   //deleteion
+
+//   Future<int> deleteUser(User user) async {
+//     var dbClient = await db;
+//     int res = await dbClient.delete("User");
+
+//     return res;
+//   }
+// }
+
+import 'dart:io';
+import 'package:sqlite_login/models/user.dart';
+import 'package:path/path.dart';
 import 'dart:async';
-import 'dart:io' as io;
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper _instance = new DatabaseHelper.internal();
-
   factory DatabaseHelper() => _instance;
+
   static Database _db;
 
   Future<Database> get db async {
@@ -16,14 +72,32 @@ class DatabaseHelper {
     return _db;
   }
 
-  DatabaseHelper.internal();  
+  DatabaseHelper.internal();
 
   initDb() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, "main.db");
+    var ourDb = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return ourDb;
+  }
 
-    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentDirectory.path, 'main.db');
+  void _onCreate(Database db, int version) async {
+    await db.execute(
+        "CREATE TABLE User(id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
+    print("Table is created");
+  }
 
-    var ourDb =  await openDatabase(path,version: 1,oncreate: _onCreate);
-      
+//insertion
+  Future<int> saveUser(User user) async {
+    var dbClient = await db;
+    int res = await dbClient.insert("User", user.toMap());
+    return res;
+  }
+
+  //deletion
+  Future<int> deleteUser(User user) async {
+    var dbClient = await db;
+    int res = await dbClient.delete("User");
+    return res;
   }
 }
