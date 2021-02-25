75fe034098f3145dce0c085ac5b687a5f836a3ce
namancoder <namanitsofficial@gmail.com>
2021-02-25 11:35:59 +0530

    3rd

--- SQLITE_MVP/sqlite_login/lib/main.dart
+++ SQLITE_MVP/sqlite_login/lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:sqlite_login/pages/home_page.dart';
+
+import 'pages/login/login_page.dart';
+
 
 void main() => runApp(new MyApp());
 
 
 final routes = {
-  '/login': (BuildContext context)=> new LoginPage();
-  '/home': (BuildContext context)=> new HomePage(); 
-   '/': (BuildContext context)=> new LoginPage();
-
-
-
+  '/login': (BuildContext context)=> new LoginPage(),
+  '/home': (BuildContext context)=> new HomePage(),
+   '/': (BuildContext context)=> new LoginPage(),
 };
 class MyApp extends StatelessWidget {
   @override
