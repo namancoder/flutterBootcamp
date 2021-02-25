0784297183273ab3960a2a9672e99127302b7daa
namancoder <namanitsofficial@gmail.com>
2021-02-25 12:02:30 +0530

    post final

--- SQLITE_MVP/sqlite_login/lib/models/user.dart
+++ SQLITE_MVP/sqlite_login/lib/models/user.dart
@@ -13,7 +13,7 @@ class User {
   Map<String, dynamic> toMap() {
     var map = new Map<String, dynamic>();
     map['username'] = _username;
-    map['password'] -= _password;
+    map['password'] = _password;
 
     return map;
   }
